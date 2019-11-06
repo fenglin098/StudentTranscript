78c33b7a799e780f11d8aa5aa492e623d9618379
fenglin098 <feng.lin098@gmail.com>
2019-11-06 12:00:11 -0500

    second commit

--- src/TranscriptApp.java
+++ src/TranscriptApp.java
@@ -10,15 +10,31 @@ public class TranscriptApp {
         Student st1 = new Student();
         st1.setStudentID(1);
         st1.setStudentName("John Doe");
-        ArrayList<Course> jdcs1 = new ArrayList<>();
-        Course cs1 = new Course();
-        cs1.setCourseID(101);
-        cs1.setCourseTitle("Java 101");
-        cs1.setCredits(3);
-        cs1.setGrade("A");
-        jdcs1.add(cs1);
+        ArrayList<Course> jdclasses = new ArrayList<>();
+        Course jd1 = new Course();
+        jd1.setCourseID(101);
+        jd1.setCourseTitle("Java 101");
+        jd1.setCredits(3);
+        jd1.setGrade("A");
+        jdclasses.add(jd1);
+        st1.setClasses(jdclasses);
 
-        st1.setClasses(jdcs1);
+        Course jd2 = new Course();
+        jd2.setCourseID(102);
+        jd2.setCourseTitle("English 101");
+        jd2.setCredits(3);
+        jd2.setGrade("B");
+        jdclasses.add(jd2);
+        st1.setClasses(jdclasses);
+
+        Student st2 = new Student();
+        st2.setStudentID(2);
+        st2.setStudentName("Susan Young");
+        ArrayList<Course> sycs= new ArrayList<>();
+        Course
+
+
+
 
 
  public Course (int courseID,String courseTitle,int credits,String grade){
@@ -26,6 +42,7 @@ public class TranscriptApp {
         private int studentID;
         private String studentName;
         private ArrayList<Course> classes;
+
         private int courseID;
         private String courseTitle;
         private int credits;
